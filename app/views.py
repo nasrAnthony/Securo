21c27144d80ab68640d605729dfdbec222cd7f99
nasrAnthony <anthonynasr29@gmail.com>
2025-07-27 18:28:09 -0400

    Setting up basic pages. Added stuff to gitignore, removed unused file.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# app/views.py
+
+def home(request):
+    return HttpResponse("Welcome to the Securo Home page!")
+
+def contact(request):
+    return HttpResponse("Welcome to the Securo Contact page!")
+
+def about(request):
+    return HttpResponse("Welcome to the Securo About page!")
